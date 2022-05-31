166d4c06b14c152e1cd5f48a9c3978d867268172
Ogai-Ahmadi <106423615+Ogai-Ahmadi@users.noreply.github.com>
2022-05-31 16:36:41 -0700

    first commit

---
+++ src/githubproject/GitHub.java
@@ -0,0 +1,19 @@
+package githubproject;
+
+public class GitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+char [] letter = {'A','B','C','D','E','F'};
+		
+		//for(int i = letter.length;i>=1;i--) {
+			//System.out.print(letter[i-1]+" ");
+		for(int i = letter.length-1;i>=0;i--) {
+		System.out.print(letter[i]+" ");
+		System.out.print("github testing!");
+		}
+		
+
+	}
+
+}
