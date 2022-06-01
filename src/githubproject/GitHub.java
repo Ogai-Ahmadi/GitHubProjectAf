1a4f3489b5b1e7bed3b928c1b7e8c8fc559007b1
Ogai-Ahmadi <106423615+Ogai-Ahmadi@users.noreply.github.com>
2022-05-31 17:55:22 -0700

    second commit

--- src/githubproject/GitHub.java
+++ src/githubproject/GitHub.java
@@ -11,6 +11,8 @@ char [] letter = {'A','B','C','D','E','F'};
 		for(int i = letter.length-1;i>=0;i--) {
 		System.out.print(letter[i]+" ");
 		System.out.print("github testing!");
+		System.out.print("github testing!");
+		System.out.print("github testing!");
 		}
 		
 
